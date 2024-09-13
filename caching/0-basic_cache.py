82b3ee904d37d432a4bcbc1b1e62c9c5c6ca78bd
Nyiriek <n.peat@alustudent.com>
2024-09-13 10:12:56 +0200

    Add changes

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -9,11 +9,20 @@ from base_caching import BaseCaching
 class BasicCache(BaseCaching):
     """Basic Dictionary"""
     def put(self, key, item):
+        """
+        Add the item to the cache_data dictionary with the key as the key.
+        If key or item is None, do nothing.
+        """
         if key is None or item is None:
             return
         self.cache_data[key] =  item
 
     def get(self, key):
+        """
+        Retrieve the value associated with the key from cache_data.
+        If key is None or key is not found, return None.
+        """
+
         if key is None:
             return None
         return self.cache_data.get(key, None)
