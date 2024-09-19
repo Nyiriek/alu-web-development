5aec206bd411d44b157c7409baeab68d4bc97b58
Nyiriek <n.peat@alustudent.com>
2024-09-19 14:20:40 +0200

    Basic Auth

---
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Create a class BasicAuth that inherits from Auth.
+For the moment this class will be empty.
+"""
+
+
+class BasicAuth(Auth):
+    """BasicAuth class
+    """
+    pass
