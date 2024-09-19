9158e7327f748a342a9fa965d365fc037db32340
Nyiriek <n.peat@alustudent.com>
2024-09-19 15:17:37 +0200

    Commit changes

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -30,15 +30,16 @@ class BasicAuth(Auth):
             return authorization_header[6:]
 
     def decode_base64_authorization_header(
-        self, base64_authorization_header: str
+            self,
+            base64_authorization_header: str
     ) -> str:
-        """_summary_"""
+        """decode base64 auth header"""
         if base64_authorization_header is None:
             return None
         if type(base64_authorization_header) is not str:
             return None
         try:
-            return base64.b64decode(base64_authorization_header). \
-                decode("utf-8")
+            return base64.b64decode(
+                base64_authorization_header).decode('utf-8')
         except Exception:
             return None
