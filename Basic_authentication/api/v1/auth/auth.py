9d7d70d73fa78573775ad4005200a7ae4b4547b7
Nyiriek <n.peat@alustudent.com>
2024-09-19 14:12:55 +0200

    Fix errors

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -40,7 +40,7 @@ class Auth:
         if request is None:
             return None
         if request.headers.get('Authorization') is None:
-            return None # No header
+            return None  # No header
         return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> TypeVar('User'):
