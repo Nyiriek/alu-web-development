b14ac26a7594e1e6382c5d99d215a191012554dc
Nyiriek <n.peat@alustudent.com>
2024-09-19 14:57:24 +0200

    Add changes

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -10,10 +10,9 @@ from models.user import User
 
 
 class BasicAuth(Auth):
-    """BasicAuth class
-    """
-    def extract_base64_authorization_header(self,
-                                            authorization_header: str) -> str:
+    """BasicAuth class"""
+
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
         """_summary_
 
         Args:
@@ -22,21 +21,23 @@ class BasicAuth(Auth):
         Returns:
             str: _description_
         """
-        if authorization_header is None or \
-           type(authorization_header) is not str or \
-           not authorization_header.startswith('Basic '):
+        if (
+            authorization_header is None
+            or type(authorization_header) is not str
+            or not authorization_header.startswith("Basic ")
+        ):
             return None
         return authorization_header[6:]
 
-    def decode_base64_authorization_header\
-        (self,base64_authorization_header: str) -> str:
+    def decode_base64_authorization_header(
+        self, base64_authorization_header: str
+    ) -> str:
         """_summary_"""
         if base64_authorization_header is None:
             return None
         if type(base64_authorization_header) is not str:
             return None
         try:
-            return base64.b64decode(
-                base64_authorization_header).decode('utf-8')
+            return base64.b64decode(base64_authorization_header).decode("utf-8")
         except Exception:
             return None
