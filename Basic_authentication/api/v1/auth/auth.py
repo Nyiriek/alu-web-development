c9a22f380e0e8a45989972c1c19b52ce5817c02a
Nyiriek <n.peat@alustudent.com>
2024-09-19 13:41:59 +0200

    Auth Class

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -8,10 +8,33 @@ class Auth:
     """_summary_
     """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """_summary_
+
+        Args:
+            path (str): _description_
+            excluded_paths (List[str]): _description_
+
+        Returns:
+            bool: _description_
+        """
         return False
 
     def authorization_header(self, path: str, excluded_paths: List[str]) -> str:
+        """_summary_
+
+        Args:
+            path (str): _description_
+            excluded_paths (List[str]): _description_
+
+        Returns:
+            str: _description_
+        """
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
+        """_summary_
+
+        Returns:
+            _type_: _description_
+        """
         return None
