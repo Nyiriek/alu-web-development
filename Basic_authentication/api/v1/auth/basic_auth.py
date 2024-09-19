fc39a3d3219d3ee779f6728b9a23399f97e3acdb
Nyiriek <n.peat@alustudent.com>
2024-09-19 14:55:10 +0200

    Add changes

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -28,8 +28,8 @@ class BasicAuth(Auth):
             return None
         return authorization_header[6:]
 
-    def decode_base64_authorization_header(self,
-                                           base64_authorization_header: str) -> str:
+    def decode_base64_authorization_header\
+        (self,base64_authorization_header: str) -> str:
         """_summary_"""
         if base64_authorization_header is None:
             return None
