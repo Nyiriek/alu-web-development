a5c661cc4734b8cab543cb4b0883ea84593e769c
Nyiriek <n.peat@alustudent.com>
2024-09-19 15:04:01 +0200

    commit changes

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -12,8 +12,8 @@ from models.user import User
 class BasicAuth(Auth):
     """BasicAuth class"""
 
-    def extract_base64_authorization_header (self, authorization_header: \
-        str) -> str:
+    def extract_base64_authorization_header(self, authorization_header: str) \
+        -> str:
         """_summary_
 
         Args:
